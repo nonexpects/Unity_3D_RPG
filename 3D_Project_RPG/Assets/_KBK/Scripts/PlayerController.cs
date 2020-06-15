3c1b6b1986dd3741b8c1782f7790650eb86f7abd
orionx6x <orionx6x@gmail.com>
2020-06-15 23:54:19 +0900

    유니티 프로젝트 생성
    - 버전 : 2019.3.14f1

---
+++ 3D_Project_RPG/Assets/_KBK/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    Vector3 dir;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        dir.Set(h, 0, v);
+        dir.Normalize();
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
+}
