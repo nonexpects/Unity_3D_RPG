bb038ebef25101a15b563a96094f0bace6131efd
orionx6x <orionx6x@gmail.com>
2020-06-16 17:41:51 +0900

    플레이어 점프 / 이단 점프 / Step Offset 조정

--- 3D_Project_RPG/Assets/_KBK/Scripts/PlayerController.cs
+++ 3D_Project_RPG/Assets/_KBK/Scripts/PlayerController.cs
@@ -4,20 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    CharacterController cc;
     public float speed = 5f;
+    float gravity = -10f;
+    float velocityY;
+    float jumpPower = 3f;
+    int jumpCount;
     Vector3 dir;
+
+    public VariableJoystick joystick;
+
     void Start()
     {
-        
+        cc = GetComponent<CharacterController>();
     }
-    
+
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
+        if (h == 0 && v == 0)
+        {
+            h = joystick.Horizontal;
+            v = joystick.Vertical;
+        }
+
         dir.Set(h, 0, v);
-        dir.Normalize();
-        transform.Translate(dir * speed * Time.deltaTime);
+        //dir.Normalize();
+        //dir = Camera.main.transform.TransformDirection(dir);
+        //transform.Translate(dir * speed * Time.deltaTime);
+        velocityY += gravity * Time.deltaTime;
+        dir.y = velocityY;
+        cc.Move(dir * speed * Time.deltaTime);
+
+        if (cc.collisionFlags == CollisionFlags.Below)
+        {
+            velocityY = 0f;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    public void Jump()
+    {
+        if (jumpCount > 1) return;
+
+        velocityY = jumpPower;
+        jumpCount++;
     }
 }
